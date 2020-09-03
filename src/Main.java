20afde6a247a2d5e21e7818cd69a9a179095b550
IgorP235 <igors.popovs@cognizant.com>
2020-09-03 12:42:04 +0300

    Prep for check CARD IN A ROW

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import model.Board;
 import model.Hand;
 import model.Player;
-import model.comparators.RankComparator;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -46,25 +44,46 @@ public class Main {
             playerHand[i] = new Hand(board, players[i]);
             playerHand[i].PrintHand(i);
             int result = 0, _result = 0;
+            //   RESULT             |        CHECKING ORDER       |
+            //                      |   THIRD   |  FIRST   |SECOND|
+            // 0 - High Card
+            // 1 - Pair             | Repeating |
+            // 2 - Two pairs        | Repeating |
+            // 3 - Three of a kind  | Repeating |
+            // 4 - Straight                     | In a row |
+            // 5 - Flush
+            // 6 - Full house       | Repeating |
+            // 7 - Four of a kind   | Repeating |
+            // 8 - Straight Flush               | In a row | Suit |
+            // 9 - Royal Flush                  | In a row | Suit |
 
             //IN A ROW
+            result = cardInARow(playerHand[i]);
 
             //ONE SUIT
 
-            //REPEATINGS
-            if (result < 4) _result = repeatings(playerHand[i]);
+            //REPEATING
+            //No need to check for repeating if player has Straight Flush or Royal Flush
+            if (result < 8) _result = cardRepeating(playerHand[i]);
             if (_result > result) result = _result;
         }
     }
 
-    private static int repeatings(Hand playerHand) {
+    private static int cardInARow(Hand playerHand) {
+        int result = 0;
+        
+
+        return result;
+    }
+
+    private static int cardRepeating(Hand playerHand) {
         EvaluationTests evaluationTests = new EvaluationTests();
         Map<Character, Integer> map = new HashMap<Character, Integer>();
+        int result = 0;
 
         //Check for repeating and get all repeated ranks
         map = evaluationTests.repeatrings(playerHand);
         //Define combination
-        int result = 0;
         if (map.size() != 0) result = evaluationTests.defineRepeatings(map);
         return result;
     }
